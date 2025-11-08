4b60e34987f2efc74cb039a2c255ed30ff0dfcb8
Dan1q-dev <danilaskopenko07@gmail.com>
2025-11-08 15:07:04 +0500

    Delete python-ci-lab directory

--- python-ci-lab/src/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Пакет src содержит основные модули проекта.
-"""
-from .calculator import sum_numbers, multiply_numbers, is_palindrome, calculate_rectangle_area
-
-__all__ = ['sum_numbers', 'multiply_numbers', 'is_palindrome', 'calculate_rectangle_area']
